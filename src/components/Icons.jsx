f729cb2e966f8137fde3cbf2879d1c60568780ab
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-03 01:05:05 +0530

    feat:imported icons in one file

---
+++ src/components/Icons.jsx
@@ -0,0 +1,6 @@
+import { BsArchive, BsTrash, BsPin } from "react-icons/bs";
+import { MdOutlineLabel } from "react-icons/md";
+import { VscSymbolColor,VscHome } from "react-icons/vsc";
+import { CgProfile } from "react-icons/cg";
+
+export{BsArchive, BsTrash, BsPin,MdOutlineLabel, VscSymbolColor,VscHome , CgProfile }
