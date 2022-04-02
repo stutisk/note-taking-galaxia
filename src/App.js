b68b06c54c75284be390134ef46c247c9a9e6df2
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-03 01:03:15 +0530

    feat:added Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {Home } from "./pages/Home";
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
+import {MainPage} from "./pages/MainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
     <Routes>
       <Route path="/" element={<Home />} /> 
+      <Route path="/MainPage" element={<MainPage />} /> 
+
     </Routes> 
     </div>
   );
