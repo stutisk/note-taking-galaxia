6f0ec6c8632fe5d2b22271e9c85fbd7c07a3f9e7
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-03 01:05:29 +0530

    feat:Navbar with search bar and avatar

---
+++ src/components/Mainnav.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../pages/Home.module.css";
+
+const Mainnav = () => {
+  return (
+    <>
+      <div className="container m-a">
+        <nav className="nav-bar ">
+          <div className="left-side  ">
+            <Link to="/">
+              {" "}
+              GALA<span class="span">X!A</span>
+            </Link>
+          </div>
+          <div className={styles.searchbar}>
+            <form action="#">
+              <input
+                type="search"
+                placeholder=" Search For Notes"
+                name="search"
+                   />
+            </form>
+          </div>
+          <button className={styles.btn}>
+        <Link className={styles.btn} to="./MainPage">
+          New note
+        </Link>
+      </button>
+      <div className={styles.profiledetails}>
+            <Link to="/">
+              <img
+                src="https://picsum.photos/id/1027/80/120"
+                alt="Avatar "
+                class="avatar size-large"
+              />
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+export { Mainnav };
