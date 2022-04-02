4dfd6de91508394990a08d3017c7156c70617893
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-03 01:07:03 +0530

    feat:components and pages in one file

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
-export {Navbar} from "./Navbar"
+
+export {Navbar} from "./Navbar";
+export {Mainnav} from "./Mainnav";
+export {Icons} from "./Icons";
+export {Notes} from "./Notes";
+export {Aside} from "./Aside";
