0bec453481811a0af780cc0f7adbb093d366f649
stutisk <48529714+stutisk@users.noreply.github.com>
2022-06-28 15:25:20 +0530

    chnages:canged user backend details

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Stuti",
+    lastName: "Kumari",
+    email: "stutiKumari@gmail.com",
+    password: "stutiSk123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
