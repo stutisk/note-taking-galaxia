64e969956042b64a0dd004a36aedbbd9e9c0e762
stutisk <48529714+stutisk@users.noreply.github.com>
2022-07-19 21:22:03 +0530

    feat:added login functionality

--- src/pages/AuthPage/Login.jsx
+++ src/pages/AuthPage/Login.jsx
@@ -3,16 +3,57 @@ import { Mainnav } from "../../components/Navbar/Mainnav";
 import styles from "../AuthPage/AuthPage.module.css";
 import { AiFillCaretRight } from "react-icons/ai";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
+import { useRef, useEffect } from "react";
+import { useAuth } from "../../Context/AuthContext";
+import { useLocation, useNavigate } from "react-router-dom";
 const Login = () => {
+  const useremail = useRef();
+  const userpass = useRef();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { isLogin, setLogin, Error, setError } = useAuth();
+
+  const loginHandler = async () => {
+    try {
+      const response = await axios.post(`/api/auth/login`, {
+        email: useremail.current.value,
+        password: userpass.current.value,
+      });
+
+      const userDetail = {
+        Email: response.data.foundUser.email,
+        name:
+          response.data.foundUser.firstName +
+          " " +
+          response.data.foundUser.lastName,
+      };
+      localStorage.setItem("token", response.data.encodedToken);
+      localStorage.setItem("user", JSON.stringify(userDetail));
+    } catch (error) {
+      console.log(error);
+      console.log(error.response.data.errors[0]);
+      setError({ error: error.response.data.errors[0] });
+    }
+  };
+
+  const guestLogin = () => {
+    useremail.current.value = "stutiKumari@gmail.com";
+    userpass.current.value = "stutiSk123";
+    loginHandler();
+    navigate("/mainpage");
+    console.log(useremail.current.value);
+  };
+
   return (
     <div>
-    
       <section className={styles.loginSection}>
         <div className={` m-t5 ${styles.authContainer}`} id="container">
           <div className="form-container ">
             <form action="login.html" className={` m-t ${styles.form}`}>
-              <h3 className={` m-5 ${styles.auntHeading}`}>Good To See You Again</h3>
+              <h3 className={` m-5 ${styles.auntHeading}`}>
+                Good To See You Again
+              </h3>
 
               <label for="aunth-input" className="label m-1t">
                 {" "}
@@ -22,6 +63,8 @@ const Login = () => {
                 className={` m-5 input-padding ${styles.aunthInput}`}
                 type="email"
                 placeholder="supriya"
+                ref={useremail}
+                id="username"
               />
               <label for="aunth-input" className="label m-1t">
                 {" "}
@@ -29,14 +72,22 @@ const Login = () => {
               </label>
               <input
                 className={` m-5 input-padding ${styles.aunthInput}`}
-                type="email"
+                type="password"
                 placeholder="supriya"
+                ref={userpass}
+                id="password"
               />
 
-              <button className={`m-5  btn-padding ${styles.loginBtn}`}>
+              <button
+                onClick={loginHandler}
+                className={`m-5  btn-padding ${styles.loginBtn}`}
+              >
                 Login
               </button>
-              <button className={`m-5  btn-padding ${styles.loginBtnsec}`}>
+              <button
+                onClick={guestLogin}
+                className={`m-5  btn-padding ${styles.loginBtnsec}`}
+              >
                 Login As Guest
               </button>
 
@@ -49,6 +100,9 @@ const Login = () => {
                   <AiFillCaretRight size={25} className="icon-color" />
                 </span>
               </Link>
+              <div className="inline-block   text-base text-red-700  p-2">
+                {Error}
+              </div>
             </form>
           </div>
         </div>
