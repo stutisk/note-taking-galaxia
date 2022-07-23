b0ef974b3cfec126b1fd801fddde8b967ec2b1fe
stutisk <48529714+stutisk@users.noreply.github.com>
2022-07-24 01:04:08 +0530

    added labels

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -2,6 +2,6 @@ import { BsArchive, BsTrash, BsPin } from "react-icons/bs";
 import { MdOutlineLabel,MdEditNote } from "react-icons/md";
 import { VscSymbolColor,VscHome } from "react-icons/vsc";
 import { CgProfile } from "react-icons/cg";
-import {AiFillCaretRight} from "react-icons/ai";
+import {AiFillCaretRight,AiOutlineCloseCircle} from "react-icons/ai";
 import {RiInboxArchiveLine,RiInboxUnarchiveLine,RiStickyNoteLine} from "react-icons/ri";
-export{MdEditNote,RiStickyNoteLine,BsArchive, BsTrash, BsPin,MdOutlineLabel, VscSymbolColor,VscHome , CgProfile ,AiFillCaretRight,RiInboxArchiveLine,RiInboxUnarchiveLine}
+export{AiOutlineCloseCircle,MdEditNote,RiStickyNoteLine,BsArchive, BsTrash, BsPin,MdOutlineLabel, VscSymbolColor,VscHome , CgProfile ,AiFillCaretRight,RiInboxArchiveLine,RiInboxUnarchiveLine}
--- src/pages/Labelpage/Labelpage.jsx
+++ src/pages/Labelpage/Labelpage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Aside } from "../../components/Aside/Aside";
-
-
+import styles from "../../components/Notes/Notes.module.css";
+import { AiOutlineCloseCircle } from "../../components/Icons";
 const Labelpage = () => {
   return (
     <div>
@@ -10,11 +10,34 @@ const Labelpage = () => {
           <Aside />
         </div>
         <section>
-        <div className=" checkout-header m-1t">
-            ADD LABELS
-            </div>
-           
-          
+          <div className=" checkout-header m-1t">ADD LABELS</div>
+          <ul className={` padding1  flex-column-start   ${styles.chipContainer}`}>
+            <li className={` padding1  ${styles.chip}  `}>
+              Work
+              <AiOutlineCloseCircle
+                size={26}
+                className="icon-color ml-1 pointer"
+              />
+            </li>
+            <li className={` padding1  ${styles.chip} `}>
+              Worksucks
+              <AiOutlineCloseCircle
+                size={26}
+                className="icon-color ml-1 pointer"
+              />
+            </li>
+            <li className={` padding1  ${styles.chip}  `}>
+              Work
+              <AiOutlineCloseCircle
+                size={26}
+                className="icon-color ml-1 pointer"
+              />
+            </li>
+            <li className={`   `}>
+             
+              <input  placeholder="New tag"  className={` padding1  ${styles.chip}  `}/>
+            </li>
+          </ul>
         </section>
       </div>
     </div>
